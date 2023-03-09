78b6681e82315b1d03c27aae6987916b6121e6cd
Jaime Leonardo Suncin Cruz <jaime.suncin@inabve.gob.sv>
2023-03-09 15:23:23 -0600

    docs(example): convert product and order to microservice apps

--- example/apps/order/src/main.ts
+++ example/apps/order/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 import { OrderModule } from './order.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(OrderModule);
-  await app.listen(process.env.PORT ?? 3002);
+  const port = Number.parseInt(process.env.PORT as string, 10) || 3002;
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    OrderModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port,
+      },
+    },
+  );
+
+  app.enableShutdownHooks();
+
+  await app.listen();
 }
+
 bootstrap();
--- example/apps/product/src/main.ts
+++ example/apps/product/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 import { ProductModule } from './product.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ProductModule);
-  await app.listen(process.env.PORT ?? 3001);
+  const port = Number.parseInt(process.env.PORT as string, 10) || 3001;
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    ProductModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port,
+      },
+    },
+  );
+
+  app.enableShutdownHooks();
+
+  await app.listen();
 }
+
 bootstrap();
